45dc4bdd283aa164de5ac6d8eda62e4b860c1d69
Arthur Pastel <arthur.pastel@gmail.com>
2019-12-11 19:47:40 +0100

    App implementation done

--- counterapi/application.py
+++ counterapi/application.py
@@ -1,6 +1,31 @@
+from os import getenv
+
+import redis
+
+REDIS_HOST = getenv("REDIS_HOST", default="localhost")
+REDIS_PORT = getenv("REDIS_PORT", default=6379)
+REDIS_DB = getenv("REDIS_DB", default=0)
+REDIS_KEY_PREFIX = getenv("REDIS_KEY_PREFIX", default=None)
+
+APPLICATION_PREFIX = "counter_api"
+REDIS_PREFIX = (
+    f"{REDIS_KEY_PREFIX}:{APPLICATION_PREFIX}"
+    if REDIS_KEY_PREFIX is not None
+    else APPLICATION_PREFIX
+)
+COUNTER_KEY = f"{REDIS_PREFIX}:counter"
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+
+
 def increment():
-    return "COucou"
+    r.incr(COUNTER_KEY)
 
 
 def get_value():
-    return 12
+    v = r.get(COUNTER_KEY)
+    if v is None:
+        val = 0
+    else:
+        val = int(v)
+    return {"value": val}
